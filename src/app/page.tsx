0afc4bdb3fa2805452854f2a5e6e972c7dead88d
PotassiumLover33 <bendhewer@gmail.com>
2023-07-06 16:56:55 +0100

    'message'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,9 @@ import Link from "next/link"
 export default function Home() {
   return (
   <>
+  <head>
+    <link rel="stylesheet" href="styles.css"></link>
+  </head>
   <header className="flex justify-between items-center mb-4">
     <h1 className="text-4xl">Welcome to this website!</h1>
     <Link 
@@ -13,6 +16,7 @@ export default function Home() {
       Click here to be taken to a new page
     </Link>
   </header>
+  <div className="container"></div>
   </>
   )
 }
