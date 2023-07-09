dfb4ddfd5bff7d5b8d0c8f88ac8bdc61e5508aa1
diongurung <dion-gurung@hotmail.com>
2023-07-09 20:24:18 +0100

    yo guys we're done finally LESS GHOOOOOO 😂💀

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,7 +1,130 @@
+"use client"
+
+import Link from "next/link"
+import { Bar } from "react-chartjs-2"
+import { Chart as ChartJS, registerables} from "chart.js"
+import React, { ChangeEvent, useState } from "react"
+
+ChartJS.register(
+    ...registerables
+)
+
+let label: string = "x"
+let labels: string[] = []
+let ydata: number[] = []
+
+const colour = "white"
+
+const startData: { labels: string[]; datasets: { label: string; data: number[]; backgroundColor: string; }[] } = {
+    labels: [],
+    datasets: [{
+        label: "x",
+        data: [],
+        backgroundColor: colour
+    }]
+}
+
+const options = {
+    scales: {
+        y: {
+            beginAtZero: true
+        }
+    },
+    maintainAspectRatio: false,
+    responsive: true
+}
+
 export default function Page() {
+
+    const [chartData, setChartData] = useState(startData)
+    const [previousChartData, setPreviousChartData] = useState([startData])
+
+    const [labelInputValue, setLabelInputValue] = useState("")
+    const [xInputValue, setXInputValue] = useState("")
+    const [yInputValue, setYInputValue] = useState("")
+
+    const handleLabelInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setLabelInputValue(event.target.value)
+    }
+    const handleXInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setXInputValue(event.target.value)
+    }
+    const handleYInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setYInputValue(event.target.value)
+    }
+
+    const handleDataInputSubmit = () => {
+
+        previousChartData.push(chartData)
+        const _previousChartData: any[] = previousChartData
+        setPreviousChartData(_previousChartData)
+
+        if (labelInputValue !== "") {
+            label = labelInputValue
+        }
+        if (xInputValue !== "" && yInputValue !== "") {
+            labels.push(xInputValue)
+            ydata.push(parseInt(yInputValue))
+        }
+
+        setChartData({
+            labels: labels,
+            datasets: [{
+                label: label,
+                data: ydata,
+                backgroundColor: colour
+            }]
+        })
+    }
+
+    const clear = () => {
+        setChartData(startData)
+
+        label = "x"
+        labels = []
+        ydata = []
+    }
+
     return (
         <>
-        <header>KYS!!!! :)</header>
+        <head>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.3.0/chart.min.js"></script>
+        </head>
+        <div className ="flex items-center justify-center h-screen">
+            <Link href="/">
+                <button className="bg-neutral-100 hover:bg-neutral-400 text-neutral-950 rounded-full py-2 px-5 absolute top-8 right-8">Back</button>
+            </Link>
+            <div className="w-2/3 h-2/3 lg:h-3/4 2xl:h-7/8 2xl:w-3/4 mr-5 lg:mr-10 2xl:mr-20">
+                <Bar data={chartData} options={options} className="w-full h-full"></Bar>
+            </div>  
+            <div className="flex flex-col space-y-20 max-h-160">
+                <div className="flex flex-col space-y-10">
+                    <div className="flex flex-col space-y-2 w-64 max-h-32 justify-center">
+                        <label className="text-slate-300 text-lg text-center mr-6" id="labelInput">Enter Label:</label>
+                        <input type="text" placeholder="Enter label" className="text-center rounded-full text-neutral-950 outline-none w-48 ml-4 py-1" onChange={handleLabelInputChange} />
+                    </div>
+                    <div className="flex flex-col space-y-5 max-h-128">
+                        <div className="flex flex-col space-y-1">
+                            <label className="text-slate-300 text-lg text-center mr-7 px-7">Enter Data:</label>
+                            <div className="flex flex-col space-y-2">
+                                <div className="flex flex-row space-x-2">
+                                    <label className="text-slate-300 text-xl">X:</label>
+                                    <input type="text" placeholder="Enter X" className="text-center rounded-full text-neutral-950 outline-none" id="xInput" onChange={handleXInputChange} />
+                                </div>
+                                <div className="flex flex-row space-x-2">
+                                    <label className="text-slate-300 text-xl">Y:</label>
+                                    <input type="text" placeholder="Enter Y" className="text-center rounded-full text-neutral-950 outline-none" id="yInput" onChange={handleYInputChange} />
+                                </div>
+                            </div>
+                        </div>
+                        <div className="flex flex-row space-x-3 ml-1">
+                            <button className="bg-neutral-100 hover:bg-neutral-400 text-neutral-950 rounded-full w-48 h-10 ml-3" id="inputDataButton" onClick={handleDataInputSubmit}>Input Data</button>
+                        </div>
+                    </div>
+                </div>
+                <button className="bg-neutral-100 hover:bg-neutral-400 text-neutral-950 rounded-full h-10 w-48 ml-3" id="clearButton" onClick={clear}>Clear</button>
+            </div>
+        </div>
         </>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client'; //https://stackoverflow.com/questions/74471642/nextjs-13-button-onclick-event-handlers-cannot-be-passed-to-client-componen
 import Link from "next/link"
-import React from 'react';
+import React, { ChangeEvent, useState } from "react"
 import {
   Chart as ChartJS,
   LinearScale,
@@ -13,115 +13,155 @@ import { Scatter } from 'react-chartjs-2';
 ChartJS.register(LinearScale, PointElement, LineElement, Tooltip, Legend);
 
 
-const data = {
-  labels: ['Scatter'],
-  datasets: [
-    {
-      label: 'My First dataset',
-      fill: true,
-      backgroundColor: 'rgba(190, 217, 59, 0.13)',
-      pointBorderColor: 'rgba(75,192,192,1)',
-      pointBackgroundColor: '#fff',
-      pointBorderWidth: 5,
-      pointHoverRadius: 10,
-      pointHoverBackgroundColor: 'rgba(190, 217, 59, 0.13)',
-      pointHoverBorderColor: 'rgba(220,220,220,1)',
-      pointHoverBorderWidth: 2,
-      pointRadius: 1,
-      pointHitRadius: 10,
-      data: [
-        { x: 65, y: 75 },
-        { x: 59, y: 49 },
-        { x: 80, y: 90 },
-        { x: 81, y: 29 },
-        { x: 56, y: 36 },
-        { x: 55, y: 25 },
-        { x: 40, y: 18 },
-      ]
-    }
-  ]
-};
-
 // coordinate
 interface Coordinate {
   x: number
   y: number
   }
 
+  const graphOptions = {
+    scales: {
+      y: {
+          beginAtZero: true
+      }
+  },
+    maintainAspectRatio: true,
+    responsive: true
+}
 
   // a list of coordinates eg: stores {(x1,y1), (x2,y2) }
-  const coords: Coordinate [] =[];
-
-function ButtonClick()
-{
-  const xInput = document.getElementById('x') as HTMLInputElement;
-  const yInput = document.getElementById('y') as HTMLInputElement;
-
-  const x = parseInt(yInput.value);
-  const y = parseInt(yInput.value);
-
-  if(x && y)
-  {
-   const ordinate:Coordinate = {x ,y};
-
-   coords.push(ordinate);
-   //renderGraph(coords);
+  let coords: Coordinate [] =[];
+  let generatedPoints: Coordinate[] = [];
+  const startData = {
+    label: 'Scatter',
+    datasets: [
+      {
+        type: 'scatter',
+        label: 'User Input',
+        fill: true,
+        backgroundColor: 'rgba(190, 217, 59, 0.13)',
+        pointBorderColor: 'rgba(75,192,192,1)',
+        pointBackgroundColor: '#fff',
+        pointBorderWidth: 5,
+        pointHoverRadius: 10,
+        pointHoverBackgroundColor: 'rgba(190, 217, 59, 0.13)',
+        pointHoverBorderColor: 'rgba(220,220,220,1)',
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: coords
+      },
+      {
+        label: "Regression Line Data",
+        data: generatedPoints,
+        borderColor: "green",
+        type: "line"
+      }
+    ]
   };
-
-  console.log('hello')
-  
-
- // Reset the input fields
- xInput.value = '';
- yInput.value = '';
-
-}
-
-// function to render the chart
-
-function renderGraph(coordinates: Coordinate[])
-{
-} 
-
-function linearRegression(coordinates: Coordinate[]){
-  
-  const N: number = coordinates.length; //N is the sample size / how many points there are on the graph
-  const xCoordinates: number[] = []; // list of all the x coordinates
-  const yCoordinates: number[] = []; // list of all the y coordinates
-
-  for (const {x, y} of coordinates) { // for loop pushes the x and y coordinates into their respective lists
-    xCoordinates.push(x);
-    yCoordinates.push(y);
-  }
-
-  // declaring yummy variables
-  let summationOfXY: number; let summationOfX: number; let summationOfY: number; let summationOfXSquared: number; let gradient: number; let YIntercept: number;
-
-  // I have to assign the variables values before assigning them in the for loop, I'm not sure if there's a better way of doing it there probably is tbh idk typescript :D  
-  summationOfXY = 0; summationOfX = 0; summationOfY = 0; summationOfXSquared = 0; gradient = 0; YIntercept = 0;
-
-  for (let i = 1; i <= xCoordinates.length; i++) { // for loop that does the summation of X times Y, the summation of X, the summation of Y, and the summation of X squared
-    summationOfXY = summationOfXY + (xCoordinates[i] * yCoordinates[i])
-    summationOfX = summationOfX + xCoordinates[i]
-    summationOfY = summationOfY + yCoordinates[i]
-    summationOfXSquared = summationOfXSquared + (xCoordinates[i] ** 2)
-  }
-
-  gradient = ((N * summationOfXY) - (summationOfX * summationOfY)) / ((N * summationOfXSquared) - (summationOfX ** 2)) // calculates the gradient
-  YIntercept = (summationOfY) - (gradient * summationOfX) / N
-
-  
-}
-
 
 // using tailgate
 export default function Home() {
+    const [graphData, setGraphData] = useState(startData);
+    const [previousGraphData, setPreviousGraphData] = useState([startData])
+
+    const handleDataInputSubmit = () => {
+      previousGraphData.push(graphData)
+      const _previousGraphData: any[] = previousGraphData
+      setPreviousGraphData(_previousGraphData)     
+      const xInput = document.getElementById('x') as HTMLInputElement;
+      const yInput = document.getElementById('y') as HTMLInputElement;
+      const x = parseInt(xInput.value);
+      const y = parseInt(yInput.value);
+
+      if(x && y){
+        const ordinate:Coordinate = { x, y };
+        coords.push(ordinate);
+        console.log(coords);
+      };
+  
+ // Reset the input fields
+      xInput.value = '';
+      yInput.value = '';
+
+      setGraphData({
+        label: 'Scatter',
+        datasets: [
+          {
+            type: 'scatter',
+            label: 'User Input',
+            fill: true,
+            backgroundColor: 'rgba(190, 217, 59, 0.13)',
+            pointBorderColor: 'rgba(75,192,192,1)',
+            pointBackgroundColor: '#fff',
+            pointBorderWidth: 5,
+            pointHoverRadius: 10,
+            pointHoverBackgroundColor: 'rgba(190, 217, 59, 0.13)',
+            pointHoverBorderColor: 'rgba(220,220,220,1)',
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: coords
+          },{
+            label: "Regression Line Data",
+            data: generatedPoints,
+            borderColor: "green",
+            type: "line"
+          }
+        ]
+      })  
+    }
+    const clearGraph = () => { // clear all the points duh
+      coords = []; 
+      generatedPoints = [];
+      setGraphData(startData)  
+  }
+
+  const linearRegression = () => {
+    const N: number = coords.length; //N is the sample size / how many points there are on the graph
+    const xCoordinates: number[] = []; // list of all the x coordinates
+    const yCoordinates: number[] = []; // list of all the y coordinates
+
+    for (const {x, y} of coords) { // for loop pushes the x and y coordinates into their respective lists
+      xCoordinates.push(x);
+      yCoordinates.push(y);
+    }
+
+    // declaring yummy variables
+    let summationOfXY: number; let summationOfX: number; let summationOfY: number; let summationOfXSquared: number; let gradient: number; let YIntercept: number;
+
+    // I have to assign the variables values before assigning them in the for loop, I'm not sure if there's a better way of doing it there probably is tbh idk typescript :D  
+    summationOfXY = 0; summationOfX = 0; summationOfY = 0; summationOfXSquared = 0; gradient = 0; YIntercept = 0;
+
+    for (let i = 0; i < xCoordinates.length; i++) { // for loop that does the summation of X times Y, the summation of X, the summation of Y, and the summation of X squared
+      summationOfXY = summationOfXY + (xCoordinates[i] * yCoordinates[i])
+      summationOfX = summationOfX + xCoordinates[i]
+      summationOfY = summationOfY + yCoordinates[i]
+      summationOfXSquared = summationOfXSquared + (xCoordinates[i] ** 2)
+    }
+
+    gradient = ((N * summationOfXY) - (summationOfX * summationOfY)) / ((N * summationOfXSquared) - (summationOfX ** 2)) // calculates the gradient
+    YIntercept = ((summationOfY) - (gradient * summationOfX)) / N
+
+    const lowestXCoord = 0;
+    const highestXCoord = Math.max(...xCoordinates);
+    const distance = (highestXCoord - lowestXCoord) / 9; // Divide by 9 to get 10 equal segments
+
+    for (let i = 0; i < 10; i++) {
+      const newX = lowestXCoord + distance * i;
+      const newY = gradient * newX + YIntercept;
+      generatedPoints.push({ x: newX, y: newY });
+    }
+
+   }
+
+
   return (
     <>
       <script
       src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.js">
       </script>
-    
+
       <header className="flex justify-between items-center mb-4">
         <h1 className="text-4xl">Data Visualization Project</h1>
 
@@ -130,28 +170,34 @@ export default function Home() {
           // all this makes the button look fancy and all that
           className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
         >
-          Click here to be taken to a new page
+          Discrete Data
         </Link>
       </header>
 
      
       <div className="input-field" style={{ fontSize: '1.5rem' }}>
-      <label  className="form-label">Enter your coordinates:  </label>
-  <input type="text" placeholder="x value" aria-label="x" id="x" className="w-1/4 text-center rounded text-neutral-950 outline-none"/>
-  <input type="text" placeholder="y value" aria-label="y" id = "y" className="w-1/4 text-center rounded text-neutral-950 outline-none"/>
+          <label  className="form-label">Enter your coordinates:  </label>
       </div>
-     
-      <button type="button" style={{ fontSize: '1.5rem' }} className="bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 rounded-lg ..." id="addButton" onClick={ButtonClick}>Success</button>
 
-      
-      <div>
-      <canvas id="myChart"></canvas>
+      <div className = "flex flex-col space-y-5">
+      <div className = "flex flex-col space-y-5" >
+          <input type="text" placeholder="x value" aria-label="x" id = "x" className="w-1/4 text-center rounded text-neutral-950 outline-none"/>
+          <input type="text" placeholder="y value" aria-label="y" id = "y" className="w-1/4 text-center rounded text-neutral-950 outline-none"/>
       </div>
-    
-      <div>
-        <h2>Scatter Example</h2>
+      
+      <div className = "flex flex-col space-y-5 w-32" >
+      <button type="button" style={{ fontSize: '1.5rem' }} className="bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 rounded-lg ..." id="addButton" onClick={handleDataInputSubmit}>Enter Points</button>   
+      <button type="button" style={{ fontSize: '1.5rem' }} className="bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 rounded-lg ..." id="addButton" onClick={clearGraph}>Clear</button>
+      <button type="button" style={{ fontSize: '1.5rem' }} className="bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 rounded-lg ..." id="addButton" onClick={linearRegression}>Linear Regression</button>
+      </div>
+      </div>
+
+    <canvas id = "myChart"></canvas>
+    <div>
+        <h2>Scatter Graph</h2>
         <Scatter
-          data={data}
+          data = {graphData}
+          options = {graphOptions}
           width={400}
           height={400}
         />
@@ -160,8 +206,6 @@ export default function Home() {
     </> 
   );
 };
-
-
 
 
 
